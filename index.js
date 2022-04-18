20c21d9fa2628a7b806660017e9dc990fad313e2
camilevahviraki <camilevahviraki@gmail.com>
2022-04-18 17:11:54 +0200

    Update the Js file to get the Item from LocalStorage

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let obj = {
     
 bookArr.push(obj);
 
-let bookArrId = localStorage.length+1;
+let bookArrId = localStorage.length
 localStorage.setItem('bookArrObj'+bookArrId,JSON.stringify(obj))
 
 }
@@ -26,26 +26,66 @@ localStorage.setItem('bookArrObj'+bookArrId,JSON.stringify(obj))
 
 
 
+const BookStored = [];
 
 
 function updateUi(num){
-//   const allBooks = JSON.parse(localStorage)
-  for(let i = 0; i < localStorage.length;i++){
-     console.log(localStorage[i])
+    const bookLi = document.createElement('div');
+    const buttonRemove = document.createElement('button');
+    buttonRemove.innerText = 'Remove';
+  
+  for (let i = 0; i < localStorage.length; i++) {
+    const ParsedBookData = JSON.parse(localStorage.getItem('bookArrObj'+i))
+    BookStored.push(ParsedBookData);
   }
-//   console.log(allBooks)
-// const {title} = JSON.parse();
-// const {author} = JSON.parse(localStorage.getItem('obj'));
-
-
-//     const bookLi = document.createElement('li');
+  // console.log(BookStored);
+ 
+   for(let a=0; a<BookStored.length; a++) {
+     const bookTitle = BookStored[a].title;
+     const bookAuthor = BookStored[a].author;
+     
+    bookLi.innerHTML = `<h3>${bookTitle}:</h3><h4>${bookAuthor}</h4>`
     
-//     bookLi.innerHTML = `<h3>${title}</h3> <br/>
-//     <h4>${author}</h4> `
-//     // bookLi.textContent = `${obj.title}`
+    // bookLi.textContent = `${obj.title}`
     
-//     bookList.appendChild(bookLi)
+    bookList.append(bookLi, buttonRemove);
+
+   }
+   console.log(BookStored);
+
+  
+    
+   
 
 }
+
+function displayBooks () {
+
+  const bookLi = document.createElement('div');
+    const buttonRemove = document.createElement('button');
+    buttonRemove.innerText = 'Remove';
+    var ParsedBookData;
+  
+  for (let i = 0; i < localStorage.length; i++) {
+    ParsedBookData = JSON.parse(localStorage.getItem('bookArrObj'+i))
+    BookStored.push(ParsedBookData);
+  }
+
+  for(let a=0; a<BookStored.length; a++) {
+    const bookTitle = BookStored[a].title;
+    const bookAuthor = BookStored[a].author;
+    
+    bookLi.innerHTML += `<h3>${bookTitle}:</h3><h4>${bookAuthor}</h4>`
+    // bookLi.insertAdjacentHTML('beforeend', bookLi.append(buttonRemove));
+    // console.log(buttonRemove);
+    bookLi.append(buttonRemove);
+    bookList.append(bookLi);
+    
+  }
+  
+} 
+
+
+window.addEventListener('load', displayBooks);
 
 form.addEventListener('submit',updateUi)
